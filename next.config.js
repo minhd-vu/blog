6c49a9ecb14398b92efa722c0955da9e29c5717e
minhd-vu <minhd_vu@yahoo.com>
2023-04-17 19:01:20 -0400

    update next js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  assetPrefix: isProd ? "/blog/" : "",
   images: {
     unoptimized: true,
   },
+  output: "export",
+  distDir: "out",
 };

4feb4dbecea8c8fc53e1bb084ce509211b43955f
minhd-vu <minhd_vu@yahoo.com>
2023-04-18 13:59:29 -0400

    fix: remove assetPrefix and basePath

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  assetPrefix: process.env.CI ? "/blog/" : "",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.CI ? "/blog" : "",
   output: "export",
   distDir: "out",
 };

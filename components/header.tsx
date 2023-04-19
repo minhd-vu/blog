3c108aa9b80aebde109cc23c24d0de60921cfbea
minhd-vu <minhd_vu@yahoo.com>
2023-04-19 15:53:24 -0400

    remove author, format files, change blog title

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        Blog
+        Minhimum Effort
       </Link>
-      .
     </h2>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

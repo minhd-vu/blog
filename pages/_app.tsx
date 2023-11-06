bc9c17e09f2c3a85d73a8f291bef58b637dc0aea
minhd-vu <minhd_vu@yahoo.com>
2023-11-05 21:03:59 -0500

    chore: move analytics to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }

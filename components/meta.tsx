dda77e8ab2a4c7c5e4182ee4a290a8e91897c2e8
minhd-vu <minhd_vu@yahoo.com>
2023-11-05 21:20:18 -0500

    chore: update meta

--- components/meta.tsx
+++ components/meta.tsx
@@ -32,10 +32,7 @@ const Meta = () => {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
+      <meta name="description" content={"Minhimum Effort"} />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   );
